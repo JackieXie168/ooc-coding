ebf72efe3ff244fe7e4c581631e77bd89328c00c
Tibor <Tibor@7ac8b16d-da6f-2841-a8f8-469ec185e512>
2010-11-19 22:14:00 +0000

    Signal - improving connect parameters

--- trunk/libs/signal.h
+++ trunk/libs/signal.h
@@ -10,10 +10,12 @@ Virtuals( Signal, List )
 EndOfVirtuals;
 
 typedef void ( * SignalHandler )( void * target, void * source, void * param );
+typedef Signal * (* SignalPGetter) ( void * source );
 
-void		signal_connect( void * source, Signal * signal_p, void * target, SignalHandler handler );
 
-void 		signal_disconnect( void * source, Signal * signal_p, void * target, SignalHandler handler );
+void		signal_connect( void * source, SignalPGetter getter, void * target, SignalHandler handler );
+
+void 		signal_disconnect( void * source, SignalPGetter getter, void * target, SignalHandler handler );
 
 void		signal_emit( Signal, void * parameter, ooc_destroyer param_destroy_fn );
 void		signal_emit_sync( Signal, void * parameter, ooc_destroyer param_destroy_fn );
