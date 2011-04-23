52c96d782d8e73f3e249c8491ee852c210f7c393
Tibor <Tibor@7ac8b16d-da6f-2841-a8f8-469ec185e512>
2011-04-23 21:48:50 +0000

    Software stack implementation - embrional

---
+++ branches/swstack/tools/template/unit/suite.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Running test cases as a test suite with memory leak check
+
+
+valgrind --leak-check=yes --quiet ./ooctest
+
+valgrind --leak-check=yes --quiet ./exceptiontest
+
+# This test should not be run in Valgrind because it uses unix signals
+./testcasetest
+
+valgrind --leak-check=yes --quiet ./refcountedtest
+
+valgrind --leak-check=yes --quiet ./vectortest
+
+valgrind --leak-check=yes --quiet ./listtest
+
+valgrind --leak-check=yes --quiet ./signaltest
