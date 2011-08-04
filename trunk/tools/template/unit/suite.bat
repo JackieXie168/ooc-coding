dd1e6b19143289c5cd236fba3b1e3fc299345fc9
old-fashioned <old-fashioned@7ac8b16d-da6f-2841-a8f8-469ec185e512>
2011-08-04 21:14:49 +0000

    win32-check-all.bat created for bulk unit testing

--- trunk/tools/template/unit/suite.bat
+++ trunk/tools/template/unit/suite.bat
@@ -8,9 +8,28 @@ REM #########################################x
 
 
 @ooctest
+IF NOT %ERRORLEVEL%==0 GOTO TESTS_FAILED
+
 @exceptiontest
+IF NOT %ERRORLEVEL%==0 GOTO TESTS_FAILED
+
 REM @testcasetest
+IF NOT %ERRORLEVEL%==0 GOTO TESTS_FAILED
+
 @refcountedtest
+IF NOT %ERRORLEVEL%==0 GOTO TESTS_FAILED
+
 @vectortest
+IF NOT %ERRORLEVEL%==0 GOTO TESTS_FAILED
+
 @listtest
+IF NOT %ERRORLEVEL%==0 GOTO TESTS_FAILED
+
 @signaltest
+IF NOT %ERRORLEVEL%==0 GOTO TESTS_FAILED
+
+EXIT /B 0
+
+:TESTS_FAILED
+
+EXIT /B 1
