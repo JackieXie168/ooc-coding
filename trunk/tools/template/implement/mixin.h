9e390140900994b7c2402504107e0b2ec51b305e
old-fashioned <old-fashioned@7ac8b16d-da6f-2841-a8f8-469ec185e512>
2011-12-29 00:05:27 +0000

    some docs and templates for mixins

--- trunk/tools/template/implement/mixin.h
+++ trunk/tools/template/implement/mixin.h
@@ -5,18 +5,11 @@
 #error Do #include "sample.h" right before doing #include "implement/sample.h"
 #endif
 
-typedef
-struct
-_SampleDataFields
-{
+MixinMembers( Sample )
+
 	/* Put your mixin's data members here */
 	int		data;
 
-} SampleDataFields;
-
-extern	void	Sample_populate( Sample );
-
-extern	void	Sample_constructor( SampleData, x ); /* add additional parameters as needed! */
-extern	void	Sample_destructor( SampleData );
+EndOfMixinMembers;
 
 #endif /* IMPL_SAMPLE_H */
--- trunk/tools/template/mixin.h
+++ trunk/tools/template/mixin.h
@@ -3,14 +3,10 @@
 
 #include <ooc/ooc.h>
 
-typedef SampleDataFields * SampleData;
-
-DeclareInterface( Sample )
-
-	SampleData	(* _get_data_ )( Object );		/* Do not delete! A mixin always must have a _get_data_() method! */
+DeclareMixinInterface( Sample )
 
 	// Put your interface methods here
-	void 	(* my_method1)( Object, ... );
+	void 	(* my_method1)( Object, .. other params .. );
 
 EndOfInterface;
 
